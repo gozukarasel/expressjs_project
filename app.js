0a3a5bd269b158071ebc35725e4d189afe3480e2
gozukarasel <selim@ergcontrols.net>
2023-06-13 14:29:19 +0300

    end of section 2

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
-const http = require(`http`);
+const path = require('path');
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
-const body_parser = require('body-parser');
 
 const app = express();
 
-const path = require('path');
 
-const adminRoutes = require('./routes/admin')
-const shopRoutes = require('./routes/shop')
+app.set('view engine','ejs');
+app.set('views','views');
 
-app.use(body_parser.urlencoded({extended:false}));
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use('/admin',adminRoutes); // order matters
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
-app.use((req,res,next) =>{
-    res.status(404).sendFile(path.join(__dirname,'./','views','not-found.html'));
+app.use((req, res, next) => {
+    res.status(404).render('404',{pageTitle:'Page Not Found'});
 });
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -1,14 +1,24 @@
+const path = require('path');
+
 const express = require('express');
+
+const rootDir = require('../util/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
-const path = require('path');
+router.get('/', (req, res, next) => {
 
-router.get('/',(req,res,next) =>
-{
-    console.log("deneme");
-    res.sendFile(path.join(__dirmame,'../','views','shop.html'));
-}
-)
+  const products = adminData.products;
+
+  res.render('shop',{
+    prods:products,
+    pageTitle:'Shop',
+    path:'/',
+    hasProducts: products.length > 0,
+    activeShop:true,
+    productCss:true,  
+  })
+});
 
 module.exports = router;
